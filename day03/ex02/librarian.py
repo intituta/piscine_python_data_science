bc43ea8d2bb350e0e9ef506f70c6ac49bed6cf57
intituta <intituta@tutanota.com>
2022-09-19 18:22:42 +0300

    data_science

--- day03/ex02/librarian.py
+++ day03/ex02/librarian.py
@@ -6,7 +6,22 @@
 #    By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/19 18:13:54 by kferterb          #+#    #+#              #
-#    Updated: 2022/09/19 18:13:55 by kferterb         ###   ########.fr        #
+#    Updated: 2022/09/19 18:20:51 by kferterb         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+#!/usr/bin/env python
+
+import os
+
+
+if __name__ == '__main__':
+    print(os.environ['VIRTUAL_ENV'])
+    try:
+        if not os.environ['VIRTUAL_ENV'].endswith('kferterb'):
+            raise Exception('Incorrect env')
+    except Exception as ex:
+        print(ex)
+    else:
+        os.system('pip install beautifulsoup4 PyTest')
+        os.system('pip freeze > requirements.txt')
