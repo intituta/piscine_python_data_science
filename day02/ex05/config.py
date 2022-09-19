2e41b53486a074f3b6eb4fcd8b681363deb75389
intituta <intituta@tutanota.com>
2022-09-19 10:29:06 +0300

    data_science

--- day02/ex05/config.py
+++ day02/ex05/config.py
@@ -6,7 +6,24 @@
 #    By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/18 16:13:42 by kferterb          #+#    #+#              #
-#    Updated: 2022/09/18 16:13:43 by kferterb         ###   ########.fr        #
+#    Updated: 2022/09/19 09:53:41 by kferterb         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+def parameters():
+    file = "data.csv"
+    num_of_steps = 3
+    return file, num_of_steps
+
+def report(observation, frac_check, num_of_steps, forecast):
+    res = "Report\n"
+    res += "We have made %d observations from tossing a coin: " \
+        % (observation[0] + observation[1])
+    res += "%d of them were tails and %d of them were heads. " \
+        % (observation[0], observation[1])
+    res += "The probabilities are %0.2f%% and %0.2f%%, respectively. " \
+        % (frac_check[0], frac_check[1])
+    res += "Our forecast is that in the next %d observations " % num_of_steps
+    res += "we will have: %d tail and %d heads." \
+        % (forecast[0], forecast[1])
+    return res
--- day02/ex05/make_report.py
+++ day02/ex05/make_report.py
@@ -6,7 +6,23 @@
 #    By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/18 16:14:05 by kferterb          #+#    #+#              #
-#    Updated: 2022/09/18 16:14:06 by kferterb         ###   ########.fr        #
+#    Updated: 2022/09/19 09:53:51 by kferterb         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+from analytics import Research
+from config import parameters, report
+
+def make_report():
+    file, num_of_steps = parameters()
+    research = Research(file)
+    analytics = research.Analytics(research.file_reader())
+    observation = analytics.counts()
+    frac_check = analytics.fractions(observation)
+    forecast_list = analytics.predict_random(num_of_steps)
+    forecast = analytics.calc_random(forecast_list)
+    result = report(observation, frac_check, num_of_steps, forecast)
+    analytics.save_file(result, "report", "txt")
+
+if __name__ == '__main__':
+    make_report()
